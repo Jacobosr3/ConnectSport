7e1c7ef7985e9ee08708d97cf217ffb6f8d01568
Jacobosr3 <jacosanro@gmail.com>
2023-10-09 13:01:33 +0200

    Splash animation

--- app/src/main/java/com/example/connectsport/utilities/SplashActivity.java
+++ app/src/main/java/com/example/connectsport/utilities/SplashActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.connectsport.R;
 import com.example.connectsport.termsofservice.Fragment1;
@@ -18,6 +21,12 @@ public class SplashActivity extends AppCompatActivity {
 
         // Se establece un retraso de 2 segundos
         Handler handler = new Handler();
+        ImageView s = findViewById(R.id.s);
+        Animation myanim = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.move_s);
+        s.startAnimation(myanim);
+        ImageView c = findViewById(R.id.c);
+        Animation myanim2 = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.move_c);
+        c.startAnimation(myanim2);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
